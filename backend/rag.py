9899be930334bfe79493c8c7c53e8dd7f3544742
Rohan Harugop <rohan.harugop@gmail.com>
2025-06-29 20:36:19 +0530

    langchain import

--- backend/rag.py
+++ backend/rag.py
@@ -2,7 +2,7 @@ import os
 import json
 import torch
 from dotenv import load_dotenv
-from langchain.embeddings import HuggingFaceEmbeddings
+from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_community.vectorstores import Chroma  # Corrected import
 from langchain_openai import ChatOpenAI
 
